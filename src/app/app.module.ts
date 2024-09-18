eb4eabf800db256afe62c17ea52d2453689b538a
HoracioAF1003 <datmadness@outlook.com>
2024-09-17 20:18:18 -0600

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductlistComponent } from './product/productlist/productlist.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductlistComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/product/productlist/productlist.component.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-productlist',
+  templateUrl: './productlist.component.html',
+  styleUrl: './productlist.component.css'
+})
+export class ProductlistComponent {
+title="Saludo de variable";
+
+imageWidth:number=130;
+imageMargi:number=2;
+muestraImg:boolean=false;
+listFilter:string='';
+
+showImage():void{
+
+  this.muestraImg=!this.muestraImg;
+
+}
+
+productos:any[]=[{
+
+  "ProductoID":1,
+  "Modelo":"Sentra",
+  "Descripcion":"4 puertas, familiar",
+  "Year":"febrero 3 2022",
+  "Precio":20000,
+  "Marca":"NISSAN",
+  "Color":"Morado",
+  "ImagenURL":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSC4aiJVrVRVmW7ELV0QN5NI0nbazyUKQF89g&s"
+
+},
+{
+
+  "ProductoID":2,
+  "Modelo":"A4",
+  "Descripcion":"2 puertas",
+  "Year":"febrero 3 2023",
+  "Precio":30000,
+  "Marca":"Audi",
+  "Color":"Blanco",
+  "ImagenURL":"https://pluramotors.com.mx/site/wp-content/uploads/2023/09/Audi_A4_2022_Seminuevos_01.webp"
+
+},
+{
+
+  "ProductoID":3,
+  "Modelo":"Rio",
+  "Descripcion":"4 puertas, familiar",
+  "Year":"agosto 3 2022",
+  "Precio":60000,
+  "Marca":"KIA",
+  "Color":"Azul",
+  "ImagenURL":"https://www.eleconomista.com.mx/__export/1673897477376/sites/eleconomista/img/2023/01/16/kia_rio.jpeg_554688468.jpeg"
+
+}]
+}
